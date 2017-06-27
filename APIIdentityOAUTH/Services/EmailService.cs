8a53083935175807b83a86d6efbda07e8e07bb72
layal barakat <barbiegithub@orange.fr>
2017-06-27 21:13:11 +0200

    Version2: Send Emmail after creation Account

---
+++ APIIdentityOAUTH/Services/EmailService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.Identity;
+using SendGrid;
+
+namespace APIIdentityOAUTH.Services
+{
+
+    //l'interface "IIdentityMessageService", cette interface peut être utilisée pour configurer votre service
+    //pour envoyer des courriels ou des messages SMS, tout ce que vous devez faire est 
+    //de mettre en œuvre votre service de messagerie ou SMS dans la méthode "SendAsync "
+    public class EmailService:IIdentityMessageService
+    {
+        public  async Task SendAsync(IdentityMessage message)
+        {
+            await configSendGridAsync(message);
+        }
+
+        // Use NuGet to install SendGrid (Basic C# client lib) 
+        private async Task configSendGridAsync(IdentityMessage message)
+        {
+            var myMessage = new SendGridMessage();
+
+            myMessage.AddTo(message.Destination);
+            myMessage.From = new System.Net.Mail.MailAddress("layal_barakat@orange.fr", "layal barakat");
+            myMessage.Subject = message.Subject;
+            myMessage.Text = message.Body;
+            myMessage.Html = message.Body;
+
+            var credentials = new NetworkCredential(ConfigurationManager.AppSettings["emailService:barbiegithub"],
+                ConfigurationManager.AppSettings["emailService:haboubi123"]);
+
+            // Create a Web transport for sending email.
+            var transportWeb = new Web(credentials);
+
+            // Send the email.
+            await transportWeb.DeliverAsync(myMessage);
+        }
+    }
+}
---
+++ APIIdentityOAUTH/Validators/MyCustomUserValidator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using APIIdentityOAUTH.Infrastructure;
+using Microsoft.AspNet.Identity;
+using System.Threading.Tasks;
+
+namespace APIIdentityOAUTH.Validators
+{
+   
+        public class MyCustomUserValidator : UserValidator<ApplicationUser>
+        {
+
+            List<string> _allowedEmailDomains = new List<string> { "outlook.com", "hotmail.com", "gmail.com", "yahoo.com" };
+
+            public MyCustomUserValidator(ApplicationUserManager appUserManager)
+                : base(appUserManager)
+            {
+            }
+
+            public override async Task<IdentityResult> ValidateAsync(ApplicationUser user)
+            {
+                IdentityResult result = await base.ValidateAsync(user);
+
+                var emailDomain = user.Email.Split('@')[1];
+
+                if (!_allowedEmailDomains.Contains(emailDomain.ToLower()))
+                {
+                    var errors = result.Errors.ToList();
+
+                    errors.Add($"Email domain '{emailDomain}' is not allowed");
+
+                    result = new IdentityResult(errors);
+                }
+
+                return result;
+            }
+        }
+}
